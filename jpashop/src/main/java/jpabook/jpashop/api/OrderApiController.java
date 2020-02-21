a8cf3969c72f4410ccb2f9b3038f19da90015dc2
yjs <yjs2952@naver.com>
2020-02-21 22:10:02 +0900

    collection fetch join

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,16 @@
 package jpabook.jpashop.api;
 
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
-import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,7 +22,7 @@ public class OrderApiController {
      * 엔티티를 직접 노출한다. 절대 하지 마라
      */
     @GetMapping("/api/v1/orders")
-    public List<Order> orderV1(){
+    public List<Order> orderV1() {
         List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
 
         for (Order order : all) {
@@ -30,12 +32,66 @@ public class OrderApiController {
             List<OrderItem> orderItems = order.getOrderItems();
 
             orderItems
-                    .forEach( o -> o.getItem().getName());
+                    .forEach(o -> o.getItem().getName());
 
             // order, orderItems 강제 초기화....
             // hibernate5module 의 기본 전략이 lazy 이기 때문
         }
 
         return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> orderV2(){
+        List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> orderV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+
+        for (Order o : orders) {
+            System.out.println("order ref : " + o + " id : " + o.getId());
+        }
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(toList());
+    }
+
+    @Data
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order o) {
+            orderId = o.getId();
+            name = o.getMember().getName();
+            orderDate = o.getOrderDate();
+            orderStatus = o.getStatus();
+            address = o.getDelivery().getAddress();
+            orderItems = o.getOrderItems().stream()
+                    .map(OrderItemDto::new)
+                    .collect(toList());
+        }
+    }
+
+    @Data
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
     }
 }
--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -60,11 +60,18 @@ public class OrderRepository {
     }
 
     public List<Order> findAllWithItem(){
+        /**
+         * jpa 의 distinct는 SQL 문의 distinct 와 동일한 역할을 하며
+         * 식별자가 같은 엔티티를 걸러준다.
+         * 컬렉션을 fetch join 해버리면 페이징을 할 수 없다.
+         * 페이징을 써버리면 하이버네이트가 DB 에서 모든 데이터를 메모리로 읽어와서 그 안에서 페이징을 해 버린다. (굉장히 위험하다.)
+         */
         return em.createQuery(
-                "select o from Order o " +
-                        "join fetch o.delivery d " +
+                "select distinct o from Order o " +
                         "join fetch o.member m " +
-                        "join fetch o.orderItems oi"
+                        "join fetch o.delivery d " +
+                        "join fetch o.orderItems oi " +
+                        "join fetch oi.item", Order.class
                 ).getResultList();
     }
 }
